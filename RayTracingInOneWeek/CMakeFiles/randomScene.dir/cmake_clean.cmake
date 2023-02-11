35e990f61d7609018990cb92cc6e98a2a653a5a3
Kuwaaa <kuwaaa.rin@gmail.com>
2023-02-11 22:06:37 +0800

    Add CMakeLists

---
+++ RayTracingInOneWeek/CMakeFiles/randomScene.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/randomScene.dir/synthesis.cc.o"
+  "randomScene.pdb"
+  "randomScene"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/randomScene.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
